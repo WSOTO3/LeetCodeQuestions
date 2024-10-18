adee1cafb3b47dba2d5dae86c9e8797c88635c8c
WSOTO3 <w.soto52897@gmail.com>
2024-10-18 19:52:43 -0400

    Update Solution.py

--- Arrays&Hashing/217. Contains Duplicate/Solution.py
+++ Arrays&Hashing/217. Contains Duplicate/Solution.py
@@ -1,6 +1,8 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums)) < len(nums)
+# set removes all duplicates in a list so if the length of the set is shorter than the length of the regular list, 
+# it is TRUE that a duplicate exists, which is the Bool that the statement will return
 
 #Time Complexity: O(n)
 #Space Complexity: O(n)
