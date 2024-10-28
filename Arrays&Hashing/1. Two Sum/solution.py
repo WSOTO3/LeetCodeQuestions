15c3d930d336a964e51b43c2d7914c13f5f7e8bf
WSOTO3 <w.soto52897@gmail.com>
2024-10-27 20:20:46 -0500

    Update solution.py

--- Arrays&Hashing/1. Two Sum/solution.py
+++ Arrays&Hashing/1. Two Sum/solution.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}  # val -> index
+        prevMap = {}  # val -> index 
 
-        for i, n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
+        for i, n in enumerate(nums): #enumerate turns nums into a list of tuples for the iteration/index(i) and the element (n)
+            diff = target - n # the difference is the target number minus the current element
+            if diff in prevMap: #if the difference is in the prevMap:
+                return [prevMap[diff], i] #return the value at the index [diff] of prevMap and the value of i(its iteration/index)
+            prevMap[n] = i # if not, prevMap value at index of current value equals i 
