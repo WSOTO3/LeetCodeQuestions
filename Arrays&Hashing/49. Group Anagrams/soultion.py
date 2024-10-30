849e7ec6371778b079a1e64207dff3b0a9f00ecd
WSOTO3 <w.soto52897@gmail.com>
2024-10-29 19:34:25 -0500

    Update soultion.py

--- Arrays&Hashing/49. Group Anagrams/soultion.py
+++ Arrays&Hashing/49. Group Anagrams/soultion.py
@@ -6,9 +6,13 @@ class Solution:
             count = [0] * 26 # we will layout the 0s for each character a .....z every letter starts with 0 
             
             for c in s: #for each character in the string
-                count[ord(c) - ord("a")] += 1 # the count/occurence of that certain character goes up by 1; ord is using the ascii value of the character minus the ascii character of lower case a to get the index in count that should go up  
+                count[ord(c) - ord("a")] += 1 # the count/occurence of that certain character goes up by 1; ord is using the ascii value of
+                                              # the character minus the ascii character of lower case a to get the index in count that should
+                                              # go up  
                 
-            res[tuple(count)].append(s) #append that string as a value to the specific count key in the result dictionary; made count into a tuple since lists cannot be keys  *basically makes the tuple of 0s/1s a key and appends any word that matches with that as a value*
+            res[tuple(count)].append(s) # append that string as a value to the specific count key in the result dictionary; made count into a
+                                        # tuple since lists cannot be keys  *basically makes the tuple of 0s/1s a key and appends any word that
+                                        # matches with that as a value
         
         print(res)    
         return list(res.values())
