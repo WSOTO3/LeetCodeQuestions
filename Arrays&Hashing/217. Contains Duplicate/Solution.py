db8f831a6b13e7a262ac24cd3b38b91ffe9cab2a
WSOTO3 <w.soto52897@gmail.com>
2024-10-29 19:38:55 -0500

    Update Solution.py

--- Arrays&Hashing/217. Contains Duplicate/Solution.py
+++ Arrays&Hashing/217. Contains Duplicate/Solution.py
@@ -4,5 +4,6 @@ class Solution:
 # set removes all duplicates in a list so if the length of the set is shorter than the length of the regular list, 
 # it is TRUE that a duplicate exists, which is the Bool that the statement will return
 
-#Time Complexity: O(n)
-#Space Complexity: O(n)
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# Hash Set Length
