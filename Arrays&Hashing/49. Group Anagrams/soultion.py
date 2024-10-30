12f25dbed1b1cf6584038403da6585a8128cbc6d
WSOTO3 <w.soto52897@gmail.com>
2024-10-29 19:35:26 -0500

    Update soultion.py

--- Arrays&Hashing/49. Group Anagrams/soultion.py
+++ Arrays&Hashing/49. Group Anagrams/soultion.py
@@ -1,6 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list) # mapping charCount to list of Anagrams; defaultdict so when a key does not exist yet it doesnt give us an error 
+        res = defaultdict(list) # mapping charCount to list of Anagrams; defaultdict so when a key does not exist yet it doesnt give us an 
+                                # error
         
         for s in strs: # for every string in list of strings
             count = [0] * 26 # we will layout the 0s for each character a .....z every letter starts with 0 
@@ -10,9 +11,9 @@ class Solution:
                                               # the character minus the ascii character of lower case a to get the index in count that should
                                               # go up  
                 
-            res[tuple(count)].append(s) # append that string as a value to the specific count key in the result dictionary; made count into a
-                                        # tuple since lists cannot be keys  *basically makes the tuple of 0s/1s a key and appends any word that
-                                        # matches with that as a value
+            res[tuple(count)].append(s) # append that string as a value to the specific count key in the result dictionary; made count into 
+                                        # a tuple since lists cannot be keys  *basically makes the tuple of 0s/1s a key and appends any word 
+                                        # that matches with that as a value
         
         print(res)    
         return list(res.values())
