9dfd32210714d029f9662a517fd4f3b2b56d5349
WSOTO3 <w.soto52897@gmail.com>
2024-11-14 23:43:00 -0400

    Update solution.py

--- Arrays&Hashing/238. Product of Array Except Self/solution.py
+++ Arrays&Hashing/238. Product of Array Except Self/solution.py
@@ -5,9 +5,9 @@ class Solution:
         prefix = 1 # initialize prefix variable as 1
         for i in range(len(nums)): # looping as much times as the length of the nums list
             res[i] = prefix #result 
-            prefix *= nums[i]
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+            prefix *= nums[i] #prefix variable becomes equal to prefix times the whats in nums[i]
+        postfix = 1 # postfix is initialized at 1 
+        for i in range(len(nums) - 1, -1, -1): #looping backwards starting from the last number 
+            res[i] *= postfix # value in the position i of res list will becomes eaul to itself times postfix
+            postfix *= nums[i] # postfix becomes itself times the value in position i of nums list
+        return res # return the result
