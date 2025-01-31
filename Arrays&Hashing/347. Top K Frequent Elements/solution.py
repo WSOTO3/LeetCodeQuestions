81b3c82bf4b223029f9ce3983ab446bea0c8b329
WSOTO3 <w.soto52897@gmail.com>
2025-01-31 00:46:26 -0500

    Update solution.py

--- Arrays&Hashing/347. Top K Frequent Elements/solution.py
+++ Arrays&Hashing/347. Top K Frequent Elements/solution.py
@@ -1,15 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {} # create a dictionary for the counts for each element/number 
-        freq = [[] for i in range(len(nums) + 1)] # create a list of lists that is as long as the length of the list of nums
+        freq = [[] for i in range(len(nums) + 1)] # create a big list, the size of "nums", with lists as the items
 
-        for num in nums: # for every number in the list of numbers: 
-            count[num] = 1 + count.get(num, 0) # increase the count of the specific number by one by getting the value of the number  
-                                               # from the dictionary and adding one 
+        for num in nums: # for every number in "nums": 
+            count[num] = count.get(num, 0) + 1 # increase the count of the specific number by one by getting the value of the number(key) 
+                                               # from the dictionary and adding one; if number is not already a key, will default at 0 then
+                                               # add 1
         for num, cnt in count.items(): # for every number and count(key and value) in the count dictionary 
             freq[cnt].append(num) # add the current number to the specific list that represents the specific count 
         
         res = [] # create a list for the result/solution
+        
         for i in range(len(freq) - 1, 0, -1): # going through the freq list backwards( starting from the most frequent and going down)
             for num in freq[i]: 
                 res.append(num)  # append the number to the result/solution
