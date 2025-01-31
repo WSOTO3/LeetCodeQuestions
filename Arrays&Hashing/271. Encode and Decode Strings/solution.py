e0415e963e6f5e3c5c4095efd8dc3528d335f7ab
WSOTO3 <w.soto52897@gmail.com>
2025-01-31 01:01:56 -0500

    Update solution.py

--- Arrays&Hashing/271. Encode and Decode Strings/solution.py
+++ Arrays&Hashing/271. Encode and Decode Strings/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     
-    def encode(self, strs: List[str]) -> str:
-        res = ""
+    def encode(self, strs: List[str]) -> str: #encodes the strings by adding the number for the length of the string and "#" before it and 
+                                              #adding all strings together with no space
+        res = ""                              #example: ["cold", "hot"] -> 4#cold3#hot
         for s in strs:
             res += str(len(s)) + "#" + s
         return res
 
-    def decode(self, s: str) -> List[str]:
+    def decode(self, s: str) -> List[str]: # undos the encode function
         res = []
         i = 0
         
