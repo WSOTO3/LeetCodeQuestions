f51f535043534b216b77b7a6fc5954ce554ea333
WSOTO3 <w.soto52897@gmail.com>
2025-02-12 02:15:06 -0500

    Update solution.py

--- Arrays&Hashing/128. Longest Consecutive Sequence/solution.py
+++ Arrays&Hashing/128. Longest Consecutive Sequence/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        numSet = set(nums)
-        longest = 0
+        numSet = set(nums) # create a set for the list of numbers
+        longest = 0 # longest variable is set to zero 
 
-        for num in numSet:
-            if (num - 1) not in numSet:
-                length = 1
-                while (num + length) in numSet:
+        for num in numSet: #go through each number in the set 
+            if (num - 1) not in numSet: # if the number before it on the number line isnt in the set
+                length = 1 # length is 1
+                while (num + length) in numSet: #while number after the current number is in the set, add to the length 
                     length += 1
-                longest = max(length, longest)
-        return longest
+                longest = max(length, longest) # longest will be compared each time and the bigger number between the lcurrent ongest and 
+                                               # length variable will become the new longest 
+        return longest #return longest 
