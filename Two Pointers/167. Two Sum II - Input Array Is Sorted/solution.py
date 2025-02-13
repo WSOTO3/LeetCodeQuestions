d710fe58d41e38c7a1f07e77100f516bfdb08cae
WSOTO3 <w.soto52897@gmail.com>
2025-02-13 02:16:17 -0500

    Update solution.py

--- Two Pointers/167. Two Sum II - Input Array Is Sorted/solution.py
+++ Two Pointers/167. Two Sum II - Input Array Is Sorted/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, len(numbers) - 1
+        l, r = 0, len(numbers) - 1 #initiaalize left and right pointers
 
-        while l < r:
-            curSum = numbers[l] + numbers[r]
+        while l < r: # while left pointer hasnt reached right pointer 
+            curSum = numbers[l] + numbers[r] #current sum is value at left pointer plus the value at right pointer
 
-            if curSum > target:
-                r -= 1
-            elif curSum < target:
-                l += 1
-            else:
-                return [l + 1, r + 1]
-        return []
+            if curSum > target: # if current sum is greater than the target sum
+                r -= 1 # move the right pointer to the left by one
+            elif curSum < target: # if current sum is less than target sum
+                l += 1 # move left pointer to the right 
+            else: 
+                return [l + 1, r + 1] # return the right pointer and left pointer indices, but remember, its a 1-indexed array which means 
+                                      # the first index is indexed at 1 and not 0 how it usually is
+        
