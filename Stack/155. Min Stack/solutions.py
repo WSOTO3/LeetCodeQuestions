4c469c01ebc8893eed80764b8af939e0643e721e
WSOTO3 <w.soto52897@gmail.com>
2025-02-26 01:53:17 -0500

    Update solutions.py

--- Stack/155. Min Stack/solutions.py
+++ Stack/155. Min Stack/solutions.py
@@ -3,30 +3,30 @@ class MinStack:
         self.min = float('inf')
         self.stack = []
 
-    def push(self, x: int) -> None: #Push method
+    def push(self, x: int) -> None: # Push method
         if not self.stack: # if stack is empty
-            self.stack.append(0) #append 0
-            self.min = x #set min variable to number 
+            self.stack.append(0) # append 0
+            self.min = x # set min variable to number 
         else:
-            self.stack.append(x - self.min) #
-            if x < self.min:
-                self.min = x
+            self.stack.append(x - self.min) # append x number minus minimum
+            if x < self.min: # if x is less than min
+                self.min = x # new min is x 
 
-    def pop(self) -> None:
-        if not self.stack:
+    def pop(self) -> None: # pop methos
+        if not self.stack: # if empty, return
             return
         
-        pop = self.stack.pop()
+        pop = self.stack.pop()  # pop equals stack pop
         
-        if pop < 0:
-            self.min = self.min - pop
+        if pop < 0: # if pop is less than 0 
+            self.min = self.min - pop # min is now min minus pop 
 
-    def top(self) -> int:
-        top = self.stack[-1]
-        if top > 0:
-            return top + self.min
+    def top(self) -> int: # top method
+        top = self.stack[-1] # top is last index in stack
+        if top > 0: # if top is more than 0
+            return top + self.min # return top + min
         else:
-            return self.min
+            return self.min # return min
 
-    def getMin(self) -> int:
-        return self.min
+    def getMin(self) -> int:# min method
+        return self.min # return min
