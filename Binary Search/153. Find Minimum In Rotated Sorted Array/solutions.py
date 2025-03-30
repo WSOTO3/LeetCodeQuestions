d5db881f1eb07ddc0e8500ec86ad8506e5f11d7f
WSOTO3 <w.soto52897@gmail.com>
2025-03-30 02:20:50 -0400

    Update solutions.py

--- Binary Search/153. Find Minimum In Rotated Sorted Array/solutions.py
+++ Binary Search/153. Find Minimum In Rotated Sorted Array/solutions.py
@@ -1,10 +1,10 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
-        while l < r:
-            m = l + (r - l) // 2
-            if nums[m] < nums[r]:
-                r = m
+        l, r = 0, len(nums) - 1 #initializing the left and right pointers
+        while l < r: #while left pointer is still to the left of the right pointer 
+            m = l + (r - l) // 2 # midpoint variable is equal to value at left pointer plus (value at right minus value at left) divided by 2
+            if nums[m] < nums[r]: #if value at m is less than the value at right pointer 
+                r = m #right pointer is set to the m value
             else:
-                l = m + 1
-        return nums[l]
+                l = m + 1 # if not, left pointer is set to the value of m plus 1
+        return nums[l] # return the value at the left pointer 
