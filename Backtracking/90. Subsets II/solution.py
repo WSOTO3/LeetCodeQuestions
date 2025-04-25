af9118f6b2248386bb761a39ef3feadcd70c0c14
WSOTO3 <w.soto52897@gmail.com>
2025-04-25 19:28:21 -0400

    Create solution.py

---
+++ Backtracking/90. Subsets II/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset[::])
+                return
+
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
+        return res
+
+#Time Complexity: O(n * 2^n)
+#Space Complexity: O(n) for extra space O(2^n) for output list
